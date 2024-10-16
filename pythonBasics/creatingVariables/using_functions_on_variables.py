0de8a70adac9fe769bc0350cbde0db0fe8d9c079
AlejandroCD2k3 <alejandrocd2k3@gmail.com>
2024-10-15 21:56:44 -0500

    Updating using_functions_on_variables.py and adding string directory

--- pythonBasics/creatingVariables/using_functions_on_variables.py
+++ pythonBasics/creatingVariables/using_functions_on_variables.py
@@ -6,3 +6,17 @@ boolean_variable = True
 # Using functions
 
 print(len(string_variable)) # Printing the lenght of string_variable
+
+print(type(str(int_variable))) # str() function converts a type into its string representation
+
+# We can ask the user to input a variable
+
+first_name = input('My name is: ')
+
+# And we can convert that type if we need a specific one
+
+surname = str(input('My surname is: '))
+
+# And we can stablish a initial type
+
+address: str = "Apple avenue"
