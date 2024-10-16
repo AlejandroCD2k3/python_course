88f117256a4e3b51f0f05ba8c49f5daec2f72185
AlejandroCD2k3 <alejandrocd2k3@gmail.com>
2024-10-15 21:36:23 -0500

    Adding using_functions_on_variables.py in pythonBasics and modifying variables.py

---
+++ pythonBasics/creatingVariables/using_functions_on_variables.py
@@ -0,0 +1,8 @@
+string_variable = "This is my String variable"
+int_variable = 238
+float_variable = 15.5
+boolean_variable = True
+
+# Using functions
+
+print(len(string_variable)) # Printing the lenght of string_variable
--- pythonBasics/creatingVariables/variables.py
+++ pythonBasics/creatingVariables/variables.py
@@ -7,4 +7,15 @@ myNewVariable = "My cammel case String variable"
 my_new_variable = "My snake case String variable"
 _if = "My snake case String variable named as a reserved word"
 
+# We can also store another kind of types in our variables
+
+my_bool_variable = False
+my_int_variable = 5
+my_float_variable = 1.5
+my_complex_variable = 3 + 1j
+
+# We can add variables in just one line
+
+name, surname, alias, age = "Michael", "Bell", "Mikah", 25
+
 
