bb3b9ae87b9242e40a3f43d7293a767a2a2ecb1a
AlejandroCD2k3 <alejandrocd2k3@gmail.com>
2024-10-15 21:09:33 -0500

    Adding helloWorld in helloPython for pythonBasics

---
+++ pythonBasics/helloPython/helloWorld.py
@@ -0,0 +1,31 @@
+# Hello world code (:D)
+
+print("Hello Python! Nice to meet you")
+print('Hello world!')
+print(3)
+print(1.5)
+print(3 + 1j)
+
+print('\n We both are "str" type')
+
+print("Double quotation type: ", type("Hello Python! Nice to meet you"))
+print("Single quotation type: ", type('Hello world!'))
+
+print("\n I'm a int type")
+
+print("Int type: ", type(3))
+
+print("\n I'm a float type")
+
+print("Float type: ", type(1.5))
+
+print("\n I'm a complex type")
+
+print("Complex type: ", type(3 + 1j))
+
+
+
+
+"""
+Missed working with you!
+"""
