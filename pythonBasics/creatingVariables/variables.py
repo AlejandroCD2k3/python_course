fc0ec2328309ae8df4b3fcce450fe95133c7ccfc
AlejandroCD2k3 <alejandrocd2k3@gmail.com>
2024-10-15 21:22:31 -0500

    Adding variables.py file to pythonBasics in creatingVariables folder

---
+++ pythonBasics/creatingVariables/variables.py
@@ -0,0 +1,10 @@
+# We usually use cammel case while programming
+
+myNewVariable = "My cammel case String variable"
+
+# But for python this is not allways ok, so we use snake case instead
+
+my_new_variable = "My snake case String variable"
+_if = "My snake case String variable named as a reserved word"
+
+
