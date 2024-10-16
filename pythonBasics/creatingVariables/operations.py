90aefa44c96cc017bb6e5cc7e89042d40de37cd6
AlejandroCD2k3 <alejandrocd2k3@gmail.com>
2024-10-15 21:52:44 -0500

    Adding operations.py in pythonBasics inside creatingVariables

---
+++ pythonBasics/creatingVariables/operations.py
@@ -0,0 +1,39 @@
+# We can make operations between variables
+
+my_first_number = 5
+my_second_number = 2
+
+print(my_first_number + my_second_number) # Adding 
+print(my_first_number - my_second_number) # Subtracting
+print(my_first_number * my_second_number) # Multiplying
+print(my_first_number / my_second_number) # Dividing
+print(my_first_number % my_second_number) # Modulus
+print(my_first_number // my_second_number) # Floor division
+print(my_first_number ** my_second_number) # Power
+
+# We can also make operations with different types than just int
+
+first_word = "Hello"
+second_word = "Python"
+
+print(first_word + second_word)
+
+# We can combine strings and... strings
+
+print(first_word + str(my_first_number))
+
+# And we can make some operations with strings
+
+print(second_word * 3)
+
+# We also have logic operators
+
+print(my_first_number > my_second_number) # Greater than
+print(my_first_number < my_second_number) # Less than
+print(my_first_number >= my_second_number) # Greather than or equal to
+print(my_first_number <= my_second_number) # Greather than or equal to
+print(my_first_number == my_second_number) # Equal to
+print(my_first_number != my_second_number) # Different to
+print(my_first_number and my_second_number) # Logic and
+print(my_first_number or my_second_number) # Logic or
+print(not(my_first_number > my_second_number)) # Logic not
