785aee88e917ee204e87e46b69b74af079edf9ed
AlejandroCD2k3 <alejandrocd2k3@gmail.com>
2024-10-17 17:05:40 -0500

    Adding conditionals.py to pythonBasics in control_structures folder

---
+++ pythonBasics/control_structures/conditionals.py
@@ -0,0 +1,28 @@
+my_condition = False
+
+if my_condition: # If my condition is true (my_condition == True)
+    print("Conditional executing")
+
+print("Condition continues")
+
+#--------------------------------
+
+my_condition = 5*2
+
+if my_condition == 11: # If my condition is true (my_condition == True)
+    print("Second if conditional executing")
+
+if my_condition == 10:
+    print("Third conditional executing")
+
+#--------------------------------
+
+if my_condition > 10 and my_condition < 20:
+    print("It's greater than 10 AND less than 20")
+elif my_condition == 1:
+    print("My condition is equal to 1")
+else:
+    print("It's less or equal to 10 or greater or equal to 20")
+
+
+
