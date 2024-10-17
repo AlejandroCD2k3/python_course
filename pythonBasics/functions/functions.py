b34801dfefc94478b889279b5d410c225ec92b2f
AlejandroCD2k3 <alejandrocd2k3@gmail.com>
2024-10-17 18:56:14 -0500

    Adding functions.py to pythonBasics in functions folder

---
+++ pythonBasics/functions/functions.py
@@ -0,0 +1,9 @@
+def my_function():
+    print("This is my function")
+
+def add_two_values(first_number, second_number):
+    return first_number + second_number
+
+my_function()
+
+print(add_two_values(5,3))
