979f2702c7773f342460c9283fabc1af5bf06773
AlejandroCD2k3 <alejandrocd2k3@gmail.com>
2024-10-16 20:38:44 -0500

    Adding tuples.py to pythonBasics in data_collections folder

---
+++ pythonBasics/data_collections/tuples.py
@@ -0,0 +1,23 @@
+my_tuple = tuple() # Defyning a tuple
+my_other_tuple = () # This is a tuple too! :D
+
+my_tuple = (35, 1.77, "Michael", "Stallon")
+my_other_tuple = (28, 2.26, "Fredrick", "Last")
+
+print (my_tuple)
+print(type(my_tuple))
+print(my_tuple[0])
+print(my_tuple[-1])
+print(my_tuple.count("Michael"))
+print(my_tuple.index(35))
+
+# my_tuple[1] = 1.80    This is not possible! A tupple data can't be modified
+
+print(my_tuple+my_other_tuple)
+print(my_other_tuple[1:3])
+
+my_other_tuple = list(my_other_tuple) # We can transform a tuple into a list
+print(type(my_other_tuple))
+
+del my_other_tuple
+print(my_other_tuple)
