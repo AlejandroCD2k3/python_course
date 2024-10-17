8bdf0d09579d568551abf634da79d453151dc311
AlejandroCD2k3 <alejandrocd2k3@gmail.com>
2024-10-16 21:15:13 -0500

    Adding dictionaries.py to pythonBasics in data_collections folder

---
+++ pythonBasics/data_collections/dictionaries.py
@@ -0,0 +1,20 @@
+my_dictionary = dict()
+
+my_other_dictionary = {}
+
+print(type(my_dictionary))
+print(type(my_other_dictionary))
+
+my_other_dictionary = {"Name":"Michael", "Last name":"Jefferson", "Age":22, 1:"Python"} # We define a "Key : Value" structure
+
+my_dictionary = {
+    "Name":"Stephen",
+    "Last name":"Hunnigan",
+    "Age": 22,
+    "Languages": {"Python","Swift","Kotlin"}
+}
+
+print(my_dictionary)
+print(my_other_dictionary)
+
+
