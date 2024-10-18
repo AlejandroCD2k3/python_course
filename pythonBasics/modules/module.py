2dcf4528fc7239a93cdeb1cf5395ebf8e9a9b43a
AlejandroCD2k3 <alejandrocd2k3@gmail.com>
2024-10-17 23:22:36 -0500

    Adding modules.py and module.py to pythonBasics in modules

---
+++ pythonBasics/modules/module.py
@@ -0,0 +1,5 @@
+def add_values(numberOne, numberTwo):
+    return numberOne + numberTwo
+
+def sub_values(numberOne, numberTwo):
+    return numberOne-numberTwo
---
+++ pythonBasics/modules/modules.py
@@ -0,0 +1,7 @@
+import module
+from module import sub_values as substract
+
+print(module.add_values(1,2))
+
+print(substract(10,3))
+
