dd06a3becb38568a95ab5b93fdfff9af80d45332
AlejandroCD2k3 <alejandrocd2k3@gmail.com>
2024-10-20 20:25:35 -0500

    Updating main.py in fast_api/hello_world folder

--- pythonBackend/fast_api/hello_world/main.py
+++ pythonBackend/fast_api/hello_world/main.py
@@ -2,12 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/") # Path decorator for Root path | Local URL: http://127.0.0.1:8000/
 async def root():
     return "Hello FastAPI!"
 
-@app.get("/url")
+@app.get("/url") # Path decorator for Url path | Local URL: http:/127.0.0.1:8000/url
 
 async def url():
     return {"class_url":"https://mouredev.com/python"}
 
+
+# Starting uvicorn server: python -m uvicorn main:app --reload
+# Stopping server: CTRL + C (^C)
+
+# Swagger documentation: http://127-0.0.1:8000/docs
+# Redocly documentation: http://127-0.0.1:8000/redoc
