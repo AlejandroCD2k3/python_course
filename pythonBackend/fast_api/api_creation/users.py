80c33b73c13823dd40d3963a10186e572de52049
AlejandroCD2k3 <alejandrocd2k3@gmail.com>
2024-10-23 17:34:15 -0500

    Updating users.py in fast_api/api_creation folder

--- pythonBackend/fast_api/api_creation/users.py
+++ pythonBackend/fast_api/api_creation/users.py
@@ -3,20 +3,21 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-# Start server with: python -m users:app --reload
+# Start server with: python -m uvicorn users:app --reload
 
 # User Entity
 
 class User(BaseModel):
+    id: int
     name: str
     lastname: str
     age: int
     url: str
 
 
-users_list = [User(name="Michael",lastname="Washington",age=20, url="https://michael.dev/"),
-              User(name="Alex",lastname="Smith",age=25, url="https://alex.dev/"),
-              User(name="Ashley",lastname="Taylor",age=21, url="https://ashley.dev/")]
+users_list = [User(id = 1, name="Michael",lastname="Washington",age=20, url="https://michael.dev/"),
+              User(id = 2, name="Alex",lastname="Smith",age=25, url="https://alex.dev/"),
+              User(id = 3, name="Ashley",lastname="Taylor",age=21, url="https://ashley.dev/")]
 
 @app.get("/users_json")
 async def users_json():
@@ -25,8 +26,16 @@ async def users_json():
             {"name":"Ashley", "lastname":"Taylor", "age": 21, "url":"https://ashley.dev/"}}
 
 @app.get("/users")
-async def users():
+async def user():
     return users_list
+
+@app.get("/user/{id}")
+async def user(id: int):
+    users = filter(lambda user: user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except:
+        return "{'Error: user not found'}"
 
 """
 @app.get("users")
