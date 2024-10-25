11e420b354ccf6c31e0047f115230a014acf70af
AlejandroCD2k3 <alejandrocd2k3@gmail.com>
2024-10-24 23:27:15 -0500

    Reordering files, folders and Updating main.py

--- pythonBackend/fast_api/static_resources/main.py
+++ pythonBackend/fast_api/static_resources/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-# ----------------------- READ -----------------------
+app.mount("/static", StaticFiles(directory="static"), name="static")
