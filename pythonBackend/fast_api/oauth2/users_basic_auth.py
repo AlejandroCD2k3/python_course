44489f430ba8f8a1f96c8c180e6c238cad98306a
AlejandroCD2k3 <alejandrocd2k3@gmail.com>
2024-10-26 08:36:41 -0500

    feat: Updating users_basic_auth.py adding user  auth

--- pythonBackend/fast_api/oauth2/users_basic_auth.py
+++ pythonBackend/fast_api/oauth2/users_basic_auth.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 app = FastAPI()
+
+oauth2 = OAuth2PasswordBearer(tokenUrl="login")
 
 class User(BaseModel):
     username: str
@@ -34,3 +37,19 @@ def search_user(username: str):
     if username in users_db:
         return DBUser(users_db[username])
     return 
+
+# 
+
+@app.post("/login")
+async def login(form: OAuth2PasswordRequestForm = Depends()):
+    
+    user_db = users_db.get(form.username)
+
+    if not user_db():
+        raise HTTPException(status_code=400, detail="User not found")
+    
+    user = search_user(form.username)
+    if not form.password == user.password:
+        raise HTTPException(status_code=400, detail="Incorrect password")
+
+    return {"access_token": user.username, "token_type":"bearer"}
