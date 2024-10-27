a12d9bc4ce5c7f4d212b781c2d0050672a434a4d
AlejandroCD2k3 <alejandrocd2k3@gmail.com>
2024-10-27 07:32:23 -0500

    feat: Adding token validation in users_basic_auth.py in pythonBackend/fast_api/oauth2 folder

--- pythonBackend/fast_api/oauth2/users_basic_auth.py
+++ pythonBackend/fast_api/oauth2/users_basic_auth.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, status
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
@@ -16,7 +16,7 @@ class DBUser(User):
     password: str
 
 users_db = {
-    "Simon": {
+    "simon123": {
         "username": "simon123",
         "full_name": "Simon Lamberdt",
         "email": "simon123@gmail.com",
@@ -24,7 +24,7 @@ users_db = {
         "password": "123456"
     },
 
-    "Arthur": {
+    "arthur321": {
         "username": "arthur321",
         "full_name": "Arthur Bonnet",
         "email": "arthur321@gmail.com",
@@ -35,17 +35,22 @@ users_db = {
 
 def search_user(username: str):
     if username in users_db:
-        return DBUser(users_db[username])
-    return 
+        return DBUser(**users_db[username])
 
-# 
+async def current_user(token: str = Depends(oauth2)):
+    user = search_user(token)
+    if not user:
+        raise HTTPException(status_code= status.HTTP_401_UNAUTHORIZED, detail="Invalid auth credentials", headers={"WWW-Authenticate":"Bearer"})
+    return user
+
+# ----------------------------- CREATE -----------------------------
 
 @app.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
     
     user_db = users_db.get(form.username)
 
-    if not user_db():
+    if not user_db:
         raise HTTPException(status_code=400, detail="User not found")
     
     user = search_user(form.username)
@@ -53,3 +58,9 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
         raise HTTPException(status_code=400, detail="Incorrect password")
 
     return {"access_token": user.username, "token_type":"bearer"}
+
+# ----------------------------- READ -----------------------------
+
+@app.get("/users/me")
+async def me(user: User = Depends(current_user)):
+    return user
