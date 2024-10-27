a60ebf42ba9600b0dc4ee1684b744ce22eecfe56
AlejandroCD2k3 <alejandrocd2k3@gmail.com>
2024-10-27 07:42:57 -0500

    feat: Adding search_user function in users_basic_auth

--- pythonBackend/fast_api/oauth2/users_basic_auth.py
+++ pythonBackend/fast_api/oauth2/users_basic_auth.py
@@ -33,14 +33,22 @@ users_db = {
     }
 }
 
-def search_user(username: str):
+def search_user_db(username: str):
     if username in users_db:
         return DBUser(**users_db[username])
+    
+def search_user(username: str):
+    if username in users_db:
+        return User(**users_db[username])
 
 async def current_user(token: str = Depends(oauth2)):
     user = search_user(token)
     if not user:
         raise HTTPException(status_code= status.HTTP_401_UNAUTHORIZED, detail="Invalid auth credentials", headers={"WWW-Authenticate":"Bearer"})
+    
+    if user.disabled:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Disabled user")
+    
     return user
 
 # ----------------------------- CREATE -----------------------------
@@ -53,7 +61,7 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
     if not user_db:
         raise HTTPException(status_code=400, detail="User not found")
     
-    user = search_user(form.username)
+    user = search_user_db(form.username)
     if not form.password == user.password:
         raise HTTPException(status_code=400, detail="Incorrect password")
 
